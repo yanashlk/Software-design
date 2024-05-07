1d45fc03165d6780a9102c29b81359afcaf7a99d
YanaShuliak <yanashuliaky@gmail.com>
2024-05-07 19:20:24 +0300

    Додавання фічі за допомогою шаблоного методу 'команда'

---
+++ Lab3/MYClassLibrary5/SaveHtmlCommand.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MYClassLibrary5
+{
+    // Конкретна команда для збереження HTML-коду у файл
+    public class SaveHtmlCommand : ICommand
+    {
+        private string htmlContent;
+
+        public SaveHtmlCommand(string htmlContent)
+        {
+            this.htmlContent = htmlContent;
+        }
+
+        public void Execute()
+        {
+            File.WriteAllText("output.html", htmlContent);
+            Console.WriteLine("HTML code saved to output.html");
+        }
+    }
+}
--- Lab3/MYClassLibrary5/obj/Debug/net7.0/MYClassLibrary5.AssemblyInfo.cs
+++ Lab3/MYClassLibrary5/obj/Debug/net7.0/MYClassLibrary5.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MYClassLibrary5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e874ed63d7de12f9cee87a63be8fda12aeebe08e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75f89d2db7870393194039952c2af11b072b4ad8")]
 [assembly: System.Reflection.AssemblyProductAttribute("MYClassLibrary5")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MYClassLibrary5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
