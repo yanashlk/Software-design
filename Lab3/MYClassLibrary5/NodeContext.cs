e16170e8f78049c9e3ca666b0a659de6e1e0645b
YanaShuliak <yanashuliaky@gmail.com>
2024-05-07 19:40:29 +0300

    Використання шаблоного методу 'стейн'

---
+++ Lab3/MYClassLibrary5/NodeContext.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MYClassLibrary5
+{
+    public class NodeContext
+    {
+        private NodeState currentState;
+
+        public NodeContext()
+        {
+            // Початковий стан - блоковий
+            currentState = new BlockNodeState();
+        }
+
+        // Зміна стану на інлайновий
+        public void SwitchToInlineState()
+        {
+            currentState = new InlineNodeState();
+        }
+
+        // Зміна стану на блоковий
+        public void SwitchToBlockState()
+        {
+            currentState = new BlockNodeState();
+        }
+
+        // Метод, який використовує поточний стан
+        public void Render()
+        {
+            currentState.Render(this);
+        }
+    }
+}
